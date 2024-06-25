06f68142bb329192d0d89d8d94a98f91dcae8648
tsuchiya-sportsbull <hina.tsuchiya@sportsbull.jp>
2024-06-25 19:12:25 +0900

    ファイル拡張子の変更

---
+++ docs/index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ポートフォリオ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<style>
+* {
+    margin: 0px;
+    padding: 0px;
+}
+</style>
+
+<body>
+    <div class="header">
+        <div class="header-logo">
+            <!-- <img src="/images/bg.png"> -->
+            <img src="/images/title.png" class="header-img" alt="TSUCHIYA'S FREE BOOKS" loading="lazy">
+        </div>
+
+     <div class="introduction">    
+        作ったもの<br>描いたもの<br>色々まとめた<br>土屋の自由帳
+     </div>
+
+    </div>
+
+
+    
+    <div class="section1">
+        <div class="h1">
+        <img src="/images/illust.png" class="header-img" alt="イラスト" loading="lazy">
+        </div>   
+        
+
+        <ul class=carousel>
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/usa.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">&#128048;&#128264;&#129365;の擬人化</p>
+            </li>  
+
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/nuts.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">作品説明</p>
+            </li>  
+
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/blueberry.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">作品説明</p>
+            </li>  
+        </ul> 
+
+
+        <p class="section-text">かわいい、カラフル、ポップなイラストが得意です</p>
+      
+
+    </div>    
+
+
+
+    <div class="section-header">
+        <img src="images/while1.png" class="while"  loading="lazy">
+    </div>  
+
+
+
+    <div class="section2">
+        <div class="h1">
+        <img src="/images/logo.png" class="header-img" alt="ロゴ・作字" loading="lazy">
+        </div>  
+
+        <ul class=carousel>
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/modeling.jpg" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">&#128048;&#128264;&#129365;の擬人化</p>
+            </li>  
+
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/gokaku.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">作品説明</p>
+            </li>  
+
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/sleep.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">作品説明</p>
+            </li>  
+        </ul> 
+
+
+        <p class="section-text">かわいい、カラフル、ポップなイラストが得意です</p>
+      
+
+
+
+
+    </div> 
+
+
+
+    <div class="section-header">
+        <img src="images/while2.png" class="while"  loading="lazy">
+    </div>  
+
+
+
+    <div class="section3">
+        <div class="h1">
+        <img src="/images/tenji.png" class="header-img" alt="展示活動" loading="lazy">
+        </div>  
+
+        <ul class=carousel>
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/tsuchiya_other_02.jpg" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">&#128048;&#128264;&#129365;の擬人化</p>
+            </li>  
+
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/nuts.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">作品説明</p>
+            </li>  
+
+            <li class="item">
+                <img src="/images/page.svg" class="page-bg" alt="ページ" loading="lazy">
+                <img src="/images/blueberry.png" class="page-illust" alt="ページイラスト" loading="lazy">
+                <p class="page-text">作品説明</p>
+            </li>  
+        </ul> 
+
+
+        <p class="section-text">かわいい、カラフル、ポップなイラストが得意です</p>
+      
+
+
+
+
+
+
+
+
+
+    </div> 
+
+
+
+
+    <div class="footer">
+        <div class="self-introduction">
+            <div class="footer-section">
+                <img src="/images/self-portrait.png" class="self-portrait" alt="自画像" loading="lazy">
+                <div class="self-introduction-text">
+                    <p class="name">土屋日菜</p>
+                    <p class="detail">
+                        2001年10月31日生まれ。<br>
+                        多摩美術大学統合デザイン学科卒業。<br>
+                        かわいいものが好き。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+     <div class="copyright">    
+     © TSUCHIYA’S FREE BOOKS
+     </div>
+
+
+
+</body>
+</html>
